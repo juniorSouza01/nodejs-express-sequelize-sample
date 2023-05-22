40cd92717ed514cf0b6b1a6f0a0e3a5fd5b6ffd7
Gelado047 <juniorthesouza017@gmail.com>
2023-05-22 14:03:00 -0300

    1° Milestone

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const express = require('express');
 const app = express();
 
 app.get('/hello-world', (req, res) => {
-    let name = req.query('name');
-    if(name) {
-     res.json({ message: `Hello ${name}!` });
-    } else {
-     res.json({ message: 'Hello World' });
-    }
- });
- 
- app.get('/hello-world/:name', (req, res) => {
-     const name = req.params.name || 'World';
-     const message = `Hello ${name}!`;
-     
-     res.json({ message: message });
- });
- 
- app.listen(3000, () => {
-   console.log('Server listening on port 3000');
- });
+   let name = req.query['name'];
+   if(name) {
+    res.json({ message: `Hello ${name}!` });
+   } else {
+    res.json({ message: 'Hello World' });
+   }
+});
+
+app.get('/hello-world/:name', (req, res) => {
+    const name = req.params.name || 'World';
+    const message = `Hello ${name}!`;
+    
+    res.json({ message: message });
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
