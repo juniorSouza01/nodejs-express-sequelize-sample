1ad6e35a3a7395ef09fb7722ce0cf3a2e25643e0
Gelado047 <juniorthesouza017@gmail.com>
2023-05-23 11:26:05 -0300

    2° Milestone in progress

--- src/routes.js
+++ src/routes.js
@@ -11,4 +11,99 @@ app.get('/hello-world/:name', (req, res) => {
     res.send(`Hello ${name}!` );
 });
 
+
+
+app.post('/hello-world', (req, res) => {
+    const alunoId = Math.floor(Math.random() * 10);
+    const aluno = Object.assign({ id: alunoId }, req.body);
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            aluno
+        }
+    });
+});
+
+app.patch('/hello-world:id', (req, res) => {
+    const alunoId = req.params.id;
+    const aluno = Object.assign({ id: alunoId }, req.body);
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            aluno
+        }
+    });
+});
+
+app.delete('/hello-world:id', (req, res) => {
+    res.status(204).json({
+        status: 'success',
+        data: null,
+    });
+});
+
+app.get('/api/curso', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: {
+            cursos: [
+                {
+                    id: 1,
+                    nome: 'Curso de Node.js'
+                },
+                {
+                    id: 2,
+                    nome: 'Curso de React.js'
+                }
+            ]
+        }
+    });
+});
+
+app.get('/api/curso/:id', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: {
+            curso: {
+                id: req.params.id,
+                nome: 'Curso de React.js'
+            }
+        }
+    });
+});
+
+app.post('/api/curso', (req, res) => {
+    const cursoId = Math.floor(Math.random() * 10);
+    const curso = Object.assign({ id: cursoId }, req.body);
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            curso
+        }
+    });
+});
+
+app.patch('/api/curso/:id', (req, res) => {
+    const cursoId = req.params.id;
+    const curso = Object.assign({ id: cursoId }, req.body);
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            curso
+        }
+    });
+});
+
+app.delete('/api/curso/:id', (req, res) => {
+    res.status(204).json({
+        status: 'success',
+        data: null,
+    });
+});
+
+
 module.exports = app;
